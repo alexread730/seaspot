c376ad6d69963421b94c33a7af2337b40ae01f4e
Alex Read <alexread730@gmail.com>
2021-09-23 19:31:37 -0600

    gradient coloring

--- components/CollectionDetails.js
+++ components/CollectionDetails.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function CollectionDetails(props) {
   return (
     <div 
-      className="block grid md:grid-cols-1 md:grid-cols-3 gap-4 bg-purple-200 border-4 rounded-xl border-gray-700 shadow-md py-6 px-8 text-center"
+      className="block grid md:grid-cols-1 md:grid-cols-3 gap-4 bg-blue-500 bg-opacity-40 border-4 rounded-xl border-blue-900 shadow-md py-6 px-8 text-center"
       key={props.collection.slug}
     >
       <div className="col-span-1">
@@ -16,9 +16,9 @@ export default function CollectionDetails(props) {
         />
       </div>
       <div className="sm:col-span-2 md:col-span-2 text-center md:text-left">
-        <h3 className="text-2xl">{props.collection.name}</h3>
-        <p className="text-purple-900 font-bold text-2xl md:text-xl"><span className="text-gray-700 font-normal text-lg">Floor Price: </span>{props.collection.stats.floor_price}</p>
-        <p className="text-purple-900 font-bold text-2xl md:text-xl"><span className="text-gray-700 font-normal text-lg">Volume: </span>{Math.round(props.collection.stats.total_volume)} ETH</p>
+        <h3 className="text-2xl text-gray-900">{props.collection.name}</h3>
+        <p className="text-white font-bold text-2xl md:text-xl"><span className="text-gray-900 font-normal text-lg">Floor Price: </span>{props.collection.stats.floor_price}</p>
+        <p className="text-white font-bold text-2xl md:text-xl"><span className="text-gray-900 font-normal text-lg">Volume: </span>{Math.round(props.collection.stats.total_volume)} ETH</p>
       </div>
     </div>
   )
