b9c4af60c2f8c10d7e2b74ce9df1b7247809990c
Alex Read <alexread730@gmail.com>
2021-09-23 21:13:56 -0600

    center spinner, remove logs

--- components/CollectionsList.js
+++ components/CollectionsList.js
@@ -7,15 +7,14 @@ export default function CollectionsList() {
   const apiUrl = "https://api.opensea.io/api/v1/collections?offset=0&limit=100&asset_owner=0x453142d204dB73EeF447C763B48048DA982573D0";
   
   const { data, error } = useSWR(apiUrl, fetcher);
-  console.log(data);
   
   let content;
   if (error) {
     content = <div>failed to load</div>
   }
   
-  if (!data) {
-    content = <div className="animate-spin rounded-full border-4 border-dotted border-blue-500 h-24 w-24"></div>
+  if (true) {
+    content = <div className="col-span-3 flex justify-center"><div className="animate-spin rounded-full border-4 border-dotted border-blue-500 h-24 w-24"></div></div>
   } else {
     content = data.map((collection) => { return (
         <CollectionDetails collection={collection} key={collection.slug}/>
