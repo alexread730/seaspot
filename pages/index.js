9fb44e79039abcb67f2404ffa66c380912cdb81f
Alex Read <alexread730@gmail.com>
2021-09-27 08:38:35 -0600

    home component to class component with state, eth address state and methods

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,46 @@
 import Head from "next/head";
 import Header from "../components/Header.js";
 import CollectionsList from "../components/CollectionsList";
+import ConnectWallet from "../components/ConnectWallet";
+import React, { Component, useState } from "react";
+import { AddressContext } from "../components/AddressContext";
 
-export default function Home() {
-  return (
-    <div className="bg-gradient-to-b from-blue-400 to-green-200 min-h-screen md:min-w-full sm:min-w-full container mx-auto py-20">
-      <Head>
-        <title>SeaSpot</title>
-        <meta name="description" content="User Dashboard" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header />
-      <CollectionsList />
-      <footer></footer>
-    </div>
-  );
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        ethAddress: "",
+        setEthAddress: this.setEthAddress
+      };
+      
+    this.setEthAddress = this.setEthAddress.bind(this);
+  }
+  
+  setEthAddress = () => {
+    const res = window.ethereum.request({ method: 'eth_requestAccounts' });
+    res.then((response) => {
+      const address = response[0];
+      this.setState({ ethAddress: address });
+    })
+  };
+
+  render () {
+    return (
+      <div className="bg-gradient-to-b from-blue-400 to-green-200 min-h-screen md:min-w-full sm:min-w-full container mx-auto py-20">
+        <Head>
+          <title>SeaSpot</title>
+          <meta name="description" content="User Dashboard" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header />
+        <AddressContext.Provider value={this.state} >
+          <ConnectWallet />
+          <CollectionsList />
+        </AddressContext.Provider>
+        <footer></footer>
+      </div>
+    );
+  }
 }
+
+export default Home;
